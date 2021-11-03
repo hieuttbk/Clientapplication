b789aa03125024df001b991020bc25b45da530c0
hieuttbk <hieuttbk@gmail.com>
2021-11-03 15:33:44 +0700

    init

---
+++ src/client/app/MainControllerGUI.java
@@ -0,0 +1,342 @@
+package client.app;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.SecureRandom;
+import java.util.HashMap;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.bouncycastle.crypto.tls.BasicTlsPSKIdentity;
+import org.bouncycastle.crypto.tls.TlsClient;
+import org.bouncycastle.crypto.tls.TlsClientProtocol;
+
+import client.app.crypto.CryptographicOperations;
+import client.app.httpInterface.HTTPSpskClient;
+import client.app.httpInterface.HttpClientAuthenticationAuthorization;
+import client.app.util.Constants;
+
+public class MainControllerGUI extends JFrame {
+
+	private static final SecureRandom secureRandom = new SecureRandom();
+	
+	private static String resName;
+	private static String subType;
+	private static HashMap<String, String> aeTargets = new HashMap<String, String>();
+	
+	// Define all the components as global private variables
+	private static JLabel resLabel;
+	private static JComboBox<String> resource;
+	private static JLabel subLabel;
+	private static JComboBox<String> subscription;
+	
+	private static JButton regButton;
+	
+	private static JButton getTempButton;
+	private static JButton getHumButton;
+	private static JButton getLoudButton;
+	private static JTextField textTemp;
+	private static JTextField textHum;
+	private static JTextField textLoud;
+	private static JLabel tempUnit;
+	private static JLabel humUnit;
+	private static JLabel loudUnit;
+	
+	private static JLabel respMsg;
+	
+	public MainControllerGUI() throws IOException {
+		setTitle("Client Application");
+        setSize(1000, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        setLayout(new GridBagLayout());
+        
+        resLabel = new JLabel("Choose the resource that you want to retrieve");
+        resource = new JComboBox<String>();
+        resource.addItem("temperature");
+        resource.addItem("humidity");
+        resource.addItem("loudness");
+        
+        subLabel = new JLabel("Choose the type of subscription");
+        subscription = new JComboBox<String>();
+        subscription.addItem("silver");
+        subscription.addItem("gold");
+        subscription.addItem("platinum");
+        
+        regButton = new JButton("Register");
+        
+        regButton.addActionListener(new ActionListener() {
+        	 @Override
+             public void actionPerformed(ActionEvent evt) {
+        		 new Thread(){
+        			 public void run(){
+        				 resName = (String) resource.getSelectedItem();
+        				 subType = (String) subscription.getSelectedItem();
+
+        				 String[] response = HttpClientAuthenticationAuthorization.resourceRegistration(resName, subType).split("\\|");
+        				 String msg = response[0];
+        				 String err = response[1];
+        				 if(err.equals("false")) {
+        					 String symmetricSessionKey = HttpClientAuthenticationAuthorization.sendAuthenticationAndAuthorizationRequest();
+
+        					 // Create the HTTPS client to send encrypted requests to the OM2M Infrastructure Node
+        					 BasicTlsPSKIdentity pskIdentity = new BasicTlsPSKIdentity(Constants.clientID, symmetricSessionKey.getBytes());
+        					 HTTPSpskClient httpsClient = new HTTPSpskClient(null, pskIdentity);
+        					 InetAddress address;
+        					 int port = 5556;
+        					 try {
+        						 address = InetAddress.getLocalHost();
+        						 TlsClientProtocol protocol = openTlsConnection(address, port, httpsClient);
+        						 httpsClient.createAEclient(protocol);
+        					 } catch (UnknownHostException e) {
+        						 // TODO Auto-generated catch block
+        						 e.printStackTrace();
+        					 } catch (IOException e) {
+        						 // TODO Auto-generated catch block
+        						 e.printStackTrace();
+        					 }
+
+        					 // Save the target of the application entity corresponding to the specific resource
+        					 aeTargets.put(resName, HttpClientAuthenticationAuthorization.getAeTarget());
+        					 
+        					 if(msg.equals("empty")) {
+        						 System.out.println("Hello\n");
+        						 respMsg.setVisible(false);
+        					 }else {
+        						 System.out.println("Hello1\n");
+        						 respMsg.setText(msg);
+        						 respMsg.setVisible(true);
+        					 }
+
+        					 if(resName.equals(Constants.TEMPERATURE)) {
+        						 getTempButton.setEnabled(true);
+        					 }else if(resName.equals(Constants.HUMIDITY)) {
+        						 getHumButton.setEnabled(true);
+        					 }else if(resName.equals(Constants.LOUDNESS)) {
+        						 getLoudButton.setEnabled(true);
+        					 }
+        				 }else {
+        					 System.out.println("Hello2\n");
+        					 respMsg.setText(msg);
+        					 respMsg.setVisible(true);
+        				 }
+        				 
+        			 }
+        		 }.start();
+        	 }
+        });
+        
+        getTempButton = new JButton("Retrieve temperature");
+        getTempButton.setEnabled(false);
+        getTempButton.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent evt) {
+        		new Thread(){
+        			public void run(){
+        				String response = null;
+        				String symmetricSessionKey = CryptographicOperations.getSymmetricSessionKey();
+        				String cinTarget = aeTargets.get(Constants.TEMPERATURE) + 
+        						"/" + Constants.TEMPERATURE.toUpperCase() + "/la";
+        				// Create the HTTPS client to send encrypted requests to the OM2M Infrastructure Node
+        				BasicTlsPSKIdentity pskIdentity = new BasicTlsPSKIdentity(Constants.clientID, symmetricSessionKey.getBytes());
+        				HTTPSpskClient httpsClient = new HTTPSpskClient(null, pskIdentity);
+        				InetAddress address;
+        				int port = 5556;
+        				try {
+        					address = InetAddress.getLocalHost();
+        					TlsClientProtocol protocol = openTlsConnection(address, port, httpsClient);
+        					response = httpsClient.retrieveLatestResource(protocol, cinTarget);
+        				} catch (UnknownHostException e) {
+        					// TODO Auto-generated catch block
+        					e.printStackTrace();
+        				} catch (IOException e) {
+        					// TODO Auto-generated catch block
+        					e.printStackTrace();
+        				}
+        				
+        				if(response.length() <= 10) {
+        					textTemp.setText(response);
+        				}
+        			}
+        		}.start();
+        	}
+        });
+        
+        getHumButton = new JButton("Retrieve humidity");
+        getHumButton.setEnabled(false);
+        getHumButton.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent evt) {
+        		new Thread(){
+        			public void run(){
+        				String response = null;
+        				String symmetricSessionKey = CryptographicOperations.getSymmetricSessionKey();
+        				String cinTarget = aeTargets.get(Constants.HUMIDITY) + 
+        						"/" + Constants.HUMIDITY.toUpperCase() + "/la";
+        				// Create the HTTPS client to send encrypted requests to the OM2M Infrastructure Node
+        				BasicTlsPSKIdentity pskIdentity = new BasicTlsPSKIdentity(Constants.clientID, symmetricSessionKey.getBytes());
+        				HTTPSpskClient httpsClient = new HTTPSpskClient(null, pskIdentity);
+        				InetAddress address;
+        				int port = 5556;
+        				try {
+        					address = InetAddress.getLocalHost();
+        					TlsClientProtocol protocol = openTlsConnection(address, port, httpsClient);
+        					response = httpsClient.retrieveLatestResource(protocol, cinTarget);
+        				} catch (UnknownHostException e) {
+        					// TODO Auto-generated catch block
+        					e.printStackTrace();
+        				} catch (IOException e) {
+        					// TODO Auto-generated catch block
+        					e.printStackTrace();
+        				}
+        				
+        				if(response.length() <= 10) {
+        					textHum.setText(response);
+        				}
+        			}
+        		}.start();
+        	}
+        });
+        
+        getLoudButton = new JButton("Retrieve loudness");
+        getLoudButton.setEnabled(false);
+        getLoudButton.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent evt) {
+        		new Thread(){
+        			public void run(){
+        				String response = null;
+        				String symmetricSessionKey = CryptographicOperations.getSymmetricSessionKey();
+        				String cinTarget = aeTargets.get(Constants.LOUDNESS) + 
+        						"/" + Constants.LOUDNESS.toUpperCase() + "/la";
+        				// Create the HTTPS client to send encrypted requests to the OM2M Infrastructure Node
+        				BasicTlsPSKIdentity pskIdentity = new BasicTlsPSKIdentity(Constants.clientID, symmetricSessionKey.getBytes());
+        				HTTPSpskClient httpsClient = new HTTPSpskClient(null, pskIdentity);
+        				InetAddress address;
+        				int port = 5556;
+        				try {
+        					address = InetAddress.getLocalHost();
+        					TlsClientProtocol protocol = openTlsConnection(address, port, httpsClient);
+        					response = httpsClient.retrieveLatestResource(protocol, cinTarget);
+        				} catch (UnknownHostException e) {
+        					// TODO Auto-generated catch block
+        					e.printStackTrace();
+        				} catch (IOException e) {
+        					// TODO Auto-generated catch block
+        					e.printStackTrace();
+        				}
+        				
+        				if(response.length() <= 10) {
+        					textLoud.setText(response);
+        				}
+        			}
+        		}.start();
+        	}
+        });
+        
+        textTemp = new JTextField(10);
+        textTemp.setEditable(false);
+        textHum = new JTextField(10);
+        textHum.setEditable(false);
+        textLoud = new JTextField(10);
+        textLoud.setEditable(false);
+        
+        tempUnit = new JLabel("ºC");
+        humUnit = new JLabel("RH");
+        loudUnit = new JLabel("dB");
+        
+        respMsg = new JLabel();
+        respMsg.setForeground(Color.RED);
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 0, 10, 0);
+        add(resLabel, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        add(resource, gbc);
+        
+        gbc.gridx = 3;
+        gbc.gridy = 2;
+        add(regButton, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        add(subLabel, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        add(subscription, gbc);    
+        
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        add(respMsg, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        gbc.insets = new Insets(5, 10, 5, 10);
+        add(getTempButton, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 6;
+        add(textTemp, gbc);
+        gbc.gridx = 2;
+        gbc.gridy = 6;
+        add(tempUnit, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        add(getHumButton, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 7;
+        add(textHum, gbc);
+        gbc.gridx = 2;
+        gbc.gridy = 7;
+        add(humUnit, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 8;
+        add(getLoudButton, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 8;
+        add(textLoud, gbc);
+        gbc.gridx = 2;
+        gbc.gridy = 8;
+        add(loudUnit, gbc);
+        
+        pack();
+        setVisible(true);
+        
+	}
+	
+	public static TlsClientProtocol openTlsConnection(InetAddress address, int port, TlsClient client) throws IOException {
+        Socket s = new Socket(address, port);
+        TlsClientProtocol protocol = new TlsClientProtocol(s.getInputStream(), s.getOutputStream(), secureRandom);
+        protocol.connect(client);
+        return protocol;
+    }
+	
+	public static void main(String[] args) throws IOException {
+
+		HttpClientAuthenticationAuthorization.ECQVregistration();
+		
+		new MainControllerGUI();
+		
+	}
+
+}
---
+++ src/client/app/httpInterface/HttpClientAuthenticationAuthorization.java
@@ -0,0 +1,247 @@
+package client.app.httpInterface;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import client.app.crypto.CryptographicOperations;
+import client.app.util.Constants;
+
+public class HttpClientAuthenticationAuthorization {
+	
+	private static String ticket;
+	private static String aeTarget = null;
+	final static int port = 8080;
+
+	public static void ECQVregistration() {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		URI uri = null;
+		try {
+			uri = new URIBuilder()
+					.setScheme("http")
+					.setHost("localhost")
+					.setPort(port)
+					.setPath("/AuthorizationServer/ECQVClientRegistration")
+					.build();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String encodedU = CryptographicOperations.getUfromRandom();
+		
+		/* Create the json body for the request */
+		JsonObject jsonBody = new JsonObject();
+		jsonBody.addProperty("clientID", Constants.clientID);
+		jsonBody.addProperty("U", encodedU);
+		Gson gson = new GsonBuilder().create();
+		String body = gson.toJson(jsonBody);
+		
+		/* Create the http post request */
+		HttpPost httpPostRequest = new HttpPost(uri);
+		StringEntity entity = new StringEntity(body,
+				ContentType.create("application/json", Consts.UTF_8));
+		httpPostRequest.setEntity(entity);
+		try {
+			CloseableHttpResponse response = httpClient.execute(httpPostRequest);
+			HttpEntity respEntity = response.getEntity();
+			if(respEntity != null) {
+				String respContent = EntityUtils.toString(respEntity);
+				
+				System.out.println("Response content: " + respContent);
+				
+				/* Get the content from the json object */
+				JsonParser parser = new JsonParser();
+			    JsonObject jsonRespBody = parser.parse(respContent).getAsJsonObject();
+			    
+			    String clientCertificate = jsonRespBody.get("certificate").getAsString();
+			    String q = jsonRespBody.get("q").getAsString();
+			    String pubKeyDAS = jsonRespBody.get("pubKey") .getAsString();
+			    
+			    System.out.println("Client certificate: " + clientCertificate);
+			    System.out.println("q value: " + q);
+			    System.out.println("Public key of DAS server: " + pubKeyDAS);
+			    
+			    /* Generate the key pair */
+				CryptographicOperations.generateECKeyPair(clientCertificate, q);
+				
+				/* Verify public key correctness */
+				boolean verify = CryptographicOperations.verifyPublicKey(clientCertificate, pubKeyDAS);
+				if(verify==true) {
+					System.out.println("The client public key has been verified successfully!");
+				}else {
+					System.out.println("Closing the connection...");
+					response.close();
+					System.out.println("An attacker modified the data sent over the http channel!");
+				}
+			}
+			System.out.println("Response status code: " + response.getStatusLine().getStatusCode());
+			httpClient.close();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static String resourceRegistration(String resName, String typeSub) {
+		String msg = "empty";
+		String err = "false";
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		URI uri = null;
+		try {
+			uri = new URIBuilder()
+					.setScheme("http")
+					.setHost("localhost")
+					.setPort(port)
+					.setPath("/AuthorizationServer/ResourceClientRegistration")
+					.build();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String[] data = CryptographicOperations.generateResourceRegistraionMaterial(resName, typeSub).split("\\|");
+		
+		String Tr = data[0];
+		String subscription = data[1];
+		String nonce = data[2];
+		
+		/* Create the json body for the request */
+		JsonObject jsonBody = new JsonObject();
+		jsonBody.addProperty("clientID", Constants.clientID);
+		jsonBody.addProperty("timestamp", Tr);
+		jsonBody.addProperty("subscription", subscription);
+		jsonBody.addProperty("nonce", nonce);
+		Gson gson = new GsonBuilder().create();
+		String body = gson.toJson(jsonBody);
+		
+		System.out.println("Payload: " + body);
+		
+		/* Create the http post request */
+		HttpPost httpPostRequest = new HttpPost(uri);
+		StringEntity entity = new StringEntity(body,
+				ContentType.create("application/json", Consts.UTF_8));
+		httpPostRequest.setEntity(entity);
+		CloseableHttpResponse response;
+		try {
+			response = httpClient.execute(httpPostRequest);
+			HttpEntity respEntity = response.getEntity();
+			// Parse the response if it is not empty
+			if(respEntity != null) {
+				// Retrieve the payload
+				String respContent = EntityUtils.toString(respEntity);
+				if(response.getStatusLine().getStatusCode() != 400) {
+					// Retrieve the ticket from the payload
+					JsonParser parser = new JsonParser();
+					JsonObject jsonRespBody = parser.parse(respContent).getAsJsonObject();
+
+					if(jsonRespBody.has("message")) {
+						msg = jsonRespBody.get("message").getAsString();
+					}
+					ticket = jsonRespBody.get("ticket").getAsString();
+					System.out.println("Access ticket: " + ticket);
+				}else {
+					msg = respContent;
+					err = "true";
+					System.out.println("Error message: " + msg);
+					httpClient.close();
+					return msg;
+				}
+			}
+			System.out.println("Response status code: " + response.getStatusLine().getStatusCode());
+			httpClient.close();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return msg + "|" + err;
+	}
+	
+	public static String sendAuthenticationAndAuthorizationRequest() {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		String rxTimestamp = null;
+		String sessionKey = null;
+		URI uri = null;
+		try {
+			uri = new URIBuilder()
+					.setScheme("http")
+					.setHost("localhost")
+					.setPort(9998)
+					.build();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String Qu = CryptographicOperations.createAuthIdentity();
+		
+		/* Create the json body for the request */
+		JsonObject jsonBody = new JsonObject();
+		jsonBody.addProperty("clientID", Constants.clientID);
+		jsonBody.addProperty("Qu", Qu);
+		jsonBody.addProperty("ticket", ticket);
+		Gson gson = new GsonBuilder().create();
+		String body = gson.toJson(jsonBody);
+		
+		System.out.println("Authentication request: " + body);
+		/* Create the http post request */
+		HttpPost httpPostRequest = new HttpPost(uri);
+		StringEntity entity = new StringEntity(body,
+				ContentType.create("application/json", Consts.UTF_8));
+		httpPostRequest.setEntity(entity);
+		try {
+			CloseableHttpResponse response = httpClient.execute(httpPostRequest);
+			String responseData = EntityUtils.toString(response.getEntity());
+			EntityUtils.consume(response.getEntity());
+			System.out.println("Response: " + responseData);
+			System.out.println("Response status code: " + response.getStatusLine().getStatusCode());
+			
+			// Parse the json payload
+		    JsonParser parser = new JsonParser();
+		    JsonObject jsonReqBody = parser.parse(responseData).getAsJsonObject();
+		    
+		    // Save the timestamp Ts and the uri of the resource in variables
+		    aeTarget = jsonReqBody.get("aeTarget").getAsString();
+		    rxTimestamp = jsonReqBody.get("timestamp").getAsString();
+		    
+		    sessionKey = CryptographicOperations.generateSymmetricSessionKey(rxTimestamp);
+			
+			httpClient.close();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return sessionKey;
+	}
+	
+	public static String getAeTarget() {
+		return aeTarget;
+	}
+}
